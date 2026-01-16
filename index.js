0e4dc216d6b4a6f4d21e2f3dfabd2520113ef0b5
ax-x3 <x3amethyst@gmail.com>
2026-01-15 22:50:32 -0500

    gawsh

--- index.js
+++ index.js
@@ -2,20 +2,29 @@ export default {
     async fetch(request, env, ctx) {
         const { id: versionId, tag: versionTag, timestamp: versionTimestamp } = env.version;
 
-        // Create a JSON response with version information
+        // Set CORS headers
+        const headers = new Headers({
+            'Access-Control-Allow-Origin': 'https://[your-website]', // Replace with your website URL
+            'Access-Control-Allow-Methods': 'GET, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type',
+        });
+
+        // Handle preflight request
+        if (request.method === 'OPTIONS') {
+            return new Response(null, {
+                headers: headers,
+                status: 204, // No content
+            });
+        }
+
+        // Create a JSON response with version information for GET requests
         const versionInfo = {
             versionId,
             versionTag,
             versionTimestamp: new Date(versionTimestamp).toLocaleString()
         };
 
-        // Set CORS headers
-        const headers = new Headers({
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': 'https://aivi.party',
-            'Access-Control-Allow-Methods': 'GET',
-            'Access-Control-Allow-Headers': 'Content-Type',
-        });
+        headers.set('Content-Type', 'application/json'); // Add content type header
 
         return new Response(JSON.stringify(versionInfo), {
             headers: headers,
