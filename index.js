480207bcab41a2b057b91bba079530e8e7c4ede2
ax-x3 <x3amethyst@gmail.com>
2026-01-15 22:43:54 -0500

    agthg

--- index.js
+++ index.js
@@ -9,8 +9,16 @@ export default {
             versionTimestamp: new Date(versionTimestamp).toLocaleString()
         };
 
+        // Set CORS headers
+        const headers = new Headers({
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': 'https://aivi.party',
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Headers': 'Content-Type',
+        });
+
         return new Response(JSON.stringify(versionInfo), {
-            headers: { 'Content-Type': 'application/json' },
+            headers: headers,
         });
     },
 };
