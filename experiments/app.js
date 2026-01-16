1ecc6014f061dc91abd80983330daec10ee02fd0
ax-x3 <x3amethyst@gmail.com>
2026-01-16 01:19:07 -0500

    testing routes

--- experiments/app.js
+++ experiments/app.js
@@ -60,11 +60,14 @@ async function updateCommitInfo() {
     try {
         const res = await fetch('https://api.github.com/repos/ax-x3/aivi.party/commits/6.0');
         if (!res.ok) {
-            document.getElementById("commitId").innerHTML = "Unavailable";
+            document.getElementById("commit").innerHTML = "Unavailable";
             throw new Error("Failed to fetch.");
         }
         const resJSON = await res.json();
-        document.getElementById("commitId").innerHTML = resJSON.data.sha;
+        document.getElementById("commitId").innerHTML = " commit " + resJSON.sha;
+        const date = new Date(resJSON.commit.committer.date);
+        document.getElementById("commitDate").innerHTML = "-" + date.getFullYear() + "." + (date.getMonth() + 1) + "." + (date.getDate() + 1);
+        console.log(resJSON);
     } catch (error) {
         console.error(error);
     }   
