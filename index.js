cef7e6524d376e7f42030b95f9eba984beefc807
ax-x3 <x3amethyst@gmail.com>
2026-01-24 23:33:49 -0500

    links, scale, favicon, blog

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ async function pageNotFound() {
     <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=0.38">
         <title>404 Not Found</title>
         <script src="app.js"></script>
         <link rel="stylesheet" href="main.css">
