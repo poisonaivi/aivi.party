b4b9a49115045122a16d1716254677bb3972a278
ax-x3 <x3amethyst@gmail.com>
2026-03-02 22:45:44 -0500

    better load scripts

--- public/elements/headerScript.js
+++ public/elements/headerScript.js
@@ -1,6 +1,6 @@
+function headerLoaded() {
+    document.getElementById(path).classList.add("selected");
+
+}
 loadElement("header.html", "header");
 const path = window.location.pathname.replace(".html", "").replace("/", "");
-setTimeout(() => {
-    document.getElementById(path).classList.add("selected");
-}, 250);
-
