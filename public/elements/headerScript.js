963a8e0325773b1df34ab01100ee05e6f5e6db64
ax-x3 <x3amethyst@gmail.com>
2026-03-07 01:25:14 -0500

    big rant

--- public/elements/headerScript.js
+++ public/elements/headerScript.js
@@ -1,6 +1,6 @@
 function headerLoaded() {
+    console.log(path);
     document.getElementById(path).classList.add("selected");
-
 }
 loadElement("header.html", "header");
-const path = window.location.pathname.replace(".html", "").replace("/", "");
+const path = window.location.pathname.replace(".html", "").replace("index", "").replaceAll("/", "");
